d47db467ca538c454f68656b2f60f9f734a7bf46
XENOTEKX <velo1047@gmail.com>
2025-01-15 07:39:18 +1100

    Navbar: 1:21 (TODO)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useRef } from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    const navContainerRef = useRef(null);
+
+    return (
+        <div ref={navContainerRef} className='fixed inset-x-0 top-4 
+        z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
+            <header className='absolute top-1/2 w-full-translate-y-1/2'>
+                <nav className='flex size-full item-center justify-between
+                p-4'>
+                    <div>
+                        
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+    }
 
 export default Navbar
